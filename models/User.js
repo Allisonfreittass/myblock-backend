8f5adbe2cb325205256f6871e2122c3881b9424e
Allison Freitas <allison.freitas@alunos.unis.edu.br>
2025-08-09 16:10:04 -0300

    feat(update): adicionando melhorias no gitignore

--- models/User.js
+++ models/User.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema ({
         type: String,
         default: '',
     },
+    phone: {
+        type: String,
+        default: ''
+    },
     zipCode: {
         type: String,
         default: ''
