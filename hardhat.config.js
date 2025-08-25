6b9ef4c43514a60931e4e91d9471e37ca4fadb79
Allison Freitas <allison.freitas@alunos.unis.edu.br>
2025-08-25 19:52:01 -0300

    feat: adiciona novos estilos de contrato e deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity:    { compilers: [
+      { version: "0.8.20" },
+      { version: "0.8.28" }
+    ]},
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    localhost: { // Rede local para testes rápidos
+      url: "http://127.0.0.1:8545",
+    }
+  },
 };
